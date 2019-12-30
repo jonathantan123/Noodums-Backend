41687f5600e3f9da8e3cc74394acb405ed7a2d5e
Jonathan Tan <you@example.com>
2019-12-30 16:07:52 -0500

    updating backend

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,9 @@
-class Api::V1::OrderController < ApplicationController
+class Api::V1::OrdersController < ApplicationController
+
+
+    def show 
+        order = Order.find(params[:id])
+        render json: OrderSerializer.new(order)  
+    end 
+
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,4 @@
 class OrderSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :items
 end
